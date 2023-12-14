MiDoNaSR545/android_bootable_recovery-OFRP

commit 662efc0f9bf2ec57dcbb9e7143c939200942ca40

twrp.cpp: Fix up loading modules

twrp.cpp
@@ -119,21 +119,6 @@ static void process_fastbootd_mode() {
 		LOGINFO("starting fastboot\n");
 
 #ifdef TW_LOAD_VENDOR_MODULES
-		printf("=> Linking mtab\n");
-		symlink("/proc/mounts", "/etc/mtab");
-		std::string fstab_filename = "/etc/twrp.fstab";
-		if (!TWFunc::Path_Exists(fstab_filename)) {
-			fstab_filename = "/etc/recovery.fstab";
-		}
-		printf("=> Processing %s\n", fstab_filename.c_str());
-		if (!PartitionManager.Process_Fstab(fstab_filename, 1, false)) {
-			LOGERR("Failing out of recovery due to problem with fstab.\n");
-			return;
-		}
-		TWPartition* ven = PartitionManager.Find_Partition_By_Path("/vendor");
-		PartitionManager.Setup_Super_Devices();
-		PartitionManager.Prepare_Super_Volume(ven);
-		KernelModuleLoader::Load_Vendor_Modules();
 		if (android::base::GetBoolProperty("ro.virtual_ab.enabled", false)) {
 			PartitionManager.Unmap_Super_Devices();
 		}
@@ -153,10 +138,6 @@ static void process_fastbootd_mode() {
 static void process_recovery_mode(twrpAdbBuFifo* adb_bu_fifo, bool skip_decryption) {
 	char crash_prop_val[PROPERTY_VALUE_MAX];
 	int crash_counter;
-	std::string cmdline;
-	if (TWFunc::read_file("/proc/cmdline", cmdline) != 0) {
-		LOGINFO("Unable to read cmdline for fastboot mode\n");
-	}
 
 	property_get("orangefox.crash_counter", crash_prop_val, "-1");
 	crash_counter = atoi(crash_prop_val) + 1;
@@ -170,28 +151,8 @@ static void process_recovery_mode(twrpAdbBuFifo* adb_bu_fifo, bool skip_decrypti
 		printf("orangefox.crash_counter=%d\n", crash_counter);
 	}
 
-	printf("=> Linking mtab\n");
-	symlink("/proc/mounts", "/etc/mtab");
-	std::string fstab_filename = "/etc/twrp.fstab";
-	if (!TWFunc::Path_Exists(fstab_filename)) {
-		fstab_filename = "/etc/recovery.fstab";
-	}
-	printf("=> Processing %s\n", fstab_filename.c_str());
-	if (!PartitionManager.Process_Fstab(fstab_filename, 1, true)) {
-		LOGERR("Failing out of recovery due to problem with fstab.\n");
-		return;
-	}
-
 	// Set the props for OrangeFox dynamic partitions
 	PartitionManager.Fox_Set_Dynamic_Partition_Props(); // don't move this from here!
-
-#ifdef TW_LOAD_VENDOR_MODULES
-	bool fastboot_mode = cmdline.find("twrpfastboot=1") != std::string::npos;
-	if (fastboot_mode)
-		KernelModuleLoader::Load_Vendor_Modules();
-	else
-		KernelModuleLoader::Load_Vendor_Modules();
-#endif
 
 // We are doing this here to allow super partition to be set up prior to overriding properties
 // #if defined(TW_INCLUDE_LIBRESETPROP) && defined(TW_OVERRIDE_SYSTEM_PROPS)
@@ -470,6 +431,28 @@ int main(int argc, char **argv) {
 
 	// Load default values to set DataManager constants and handle ifdefs
 	DataManager::SetDefaultValues();
+	startupArgs startup;
+	startup.parse(&argc, &argv);
+	printf("=> Linking mtab\n");
+	symlink("/proc/mounts", "/etc/mtab");
+	std::string fstab_filename = "/etc/twrp.fstab";
+	if (!TWFunc::Path_Exists(fstab_filename)) {
+		fstab_filename = "/etc/recovery.fstab";
+	}
+	printf("=> Processing %s\n", fstab_filename.c_str());
+	if (!PartitionManager.Process_Fstab(fstab_filename, 1, !startup.Get_Fastboot_Mode())) {
+		LOGERR("Failing out of recovery due to problem with fstab.\n");
+		return -1;
+	}
+
+	// Set the props for OrangeFox dynamic partitions
+	//PartitionManager.Fox_Set_Dynamic_Partition_Props();
+
+#ifdef TW_LOAD_VENDOR_MODULES
+	if (startup.Get_Fastboot_Mode())
+		PartitionManager.Prepare_Super_Volume(PartitionManager.Find_Partition_By_Path("/vendor"));
+	KernelModuleLoader::Load_Vendor_Modules();
+#endif
 
 	// start the UI
 	printf("Starting the UI...\n");
@@ -478,8 +461,6 @@ int main(int argc, char **argv) {
 	// Load up all the resources
 	gui_loadResources();
 
-	startupArgs startup;
-	startup.parse(&argc, &argv);
 	twrpAdbBuFifo *adb_bu_fifo = new twrpAdbBuFifo();
 	TWFunc::Clear_Bootloader_Message();
 

partitionmanager.cpp
@@ -4656,12 +4656,7 @@ void TWPartitionManager::Setup_Super_Partition() {
 }
 
 bool TWPartitionManager::Get_Super_Status() {
-	std::string fastboot_mode = android::base::GetProperty("sys.usb.config", "");
-	if (fastboot_mode == "fastboot") {
-		return false;
-	}
-	else
-		return access(Get_Super_Partition().c_str(), F_OK) == 0;
+	return access(Get_Super_Partition().c_str(), F_OK) == 0;
 }
 
 bool TWPartitionManager::Recreate_Logs_Dir() {

commit eb210809b669a355216c8c7aac5292a31f338a60

twrp.cpp: Always unmap super devices on fastbootd mode proceeding Fix logical partitions flashing in fastbootd mode (was broken in 662efc0f9bf2ec57dcbb9e7143c939200942ca40) I (1)[557:fastbootd]: Fastboot command: flash:system I (1)[557:fastbootd]: [liblp]Updated logical partition table at slot 0 on device /dev/block/by-name/super E (1)[557:fastbootd]: DM_DEV_CREATE failed for [system]: Device or resource busy E (1)[557:fastbootd]: Could not map partition: system

twrp.cpp
@@ -125,11 +125,7 @@ static void Decrypt_Page(bool SkipDecryption, bool datamedia) {
 static void process_fastbootd_mode() {
 		LOGINFO("starting fastboot\n");
 
-#ifdef TW_LOAD_VENDOR_MODULES
-		if (android::base::GetBoolProperty("ro.virtual_ab.enabled", false)) {
-			PartitionManager.Unmap_Super_Devices();
-		}
-#endif
+		PartitionManager.Unmap_Super_Devices();
 
 		gui_msg(Msg("fastboot_console_msg=Entered Fastboot mode..."));
 		property_set("ro.orangefox.fastbootd", "1");